f01bdcc569d46816ed1e9d18ee7ca76c222a10dd
Krzysztof Poziomek <k.poziomek@selleo.com>
2022-02-04 14:00:15 +0100

    simple-main

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,87 @@
 import React from 'react';
+import './Home.css';
+import ReactLogo from '../Images/logo512.png';
 import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
+import {
+  Box,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  Typography,
+} from '@mui/material';
 const Home = () => {
   return (
     <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>That feels like an existential question, don't you think?</p>
-      </main>
-      <nav>
-        <Link to="/login">Login</Link>
-      </nav>
+      <Box>
+        <Card>
+          <CardHeader
+            className="nav-container"
+            title={
+              <div className="nav-logo">
+                <img className="nav-img" src={ReactLogo} alt="logos" />
+                <h3 className="nav-text">Blog-App</h3>
+              </div>
+            }
+            avatar={
+              <Button
+                className="nav-login-btn"
+                variant="contained"
+                color="primary"
+              >
+                <Link className="nav-login-link" to="/login">
+                  Login
+                </Link>
+              </Button>
+            }
+          />
+          <nav className="nav-container"></nav>
+
+          <main className="main-container">
+            <Card sx={{ maxWidth: 345 }}>
+              <Typography gutterBottom variant="h5" component="div">
+                React Blog
+              </Typography>
+              <CardMedia
+                component="img"
+                height="100"
+                image={ReactLogo}
+                alt="react blog"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  CARD ABOUT REACT
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Func fact about react
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 345 }}>
+              <Typography gutterBottom variant="h5" component="div">
+                React Blog
+              </Typography>
+              <CardMedia
+                component="img"
+                height="100"
+                image={ReactLogo}
+                alt="react blog"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  CARD ABOUT REACT
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Func fact about react
+                </Typography>
+              </CardContent>
+            </Card>
+          </main>
+          <footer className="footer-container"></footer>
+        </Card>
+      </Box>
     </>
   );
 };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,93 @@
-import React from 'react';
+/* eslint-disable no-undef */
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  Paper,
+  SvgIcon,
+  TextField,
+} from '@mui/material';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Login.css';
+// TODO: delete ?
+<Box
+  component="span"
+  sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+>
+  •
+</Box>;
+
+const HomeIcon = (props) => {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />ś
+    </SvgIcon>
+  );
+};
 
 const Login = () => {
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+
+  const onEmailChange = (e) => {
+    let emailValue = setEmailValue(e.target.value);
+    return emailValue;
+  };
+
+  const onPasswordChange = (e) => {
+    let passValue = setPasswordValue(e.target.value);
+    return passValue;
+  };
+  const handleSubmit = () => {
+    const userLogin = {
+      email: emailValue,
+      password: passwordValue,
+    };
+    setEmailValue('');
+    setPasswordValue('');
+    console.log(userLogin);
+    return userLogin;
+  };
   return (
-    <>
-      <main>
-        <h2>Welcome to the login page!</h2>
-        <p>You can login, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
+    <Box
+      className="login-page"
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      <Card sx={{ minWidth: 545 }}>
+        <CardHeader
+          className="login-card"
+          avatar={
+            <Avatar sx={{ bgcolor: '#2196f3' }} aria-label="recipe">
+              <Link to="/">
+                <HomeIcon sx={{ color: '#fff' }} />
+              </Link>
+            </Avatar>
+          }
+          title="Login Form"
+        />
+        <Paper className="login-actions">
+          <p>You can login ? I believe in you.</p>
+
+          <TextField
+            label={'Email'}
+            onChange={onEmailChange}
+            value={emailValue}
+          />
+          <TextField
+            label={'Password'}
+            type="password"
+            onChange={onPasswordChange}
+            value={passwordValue}
+          />
+
+          <Button onClick={handleSubmit}>Submit</Button>
+        </Paper>
+      </Card>
+    </Box>
   );
 };
 
