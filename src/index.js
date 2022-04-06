fc86a834a549f60518ed2b6e7202cdd355fbd9ed
Krzysztof.Poziomek <k.poziomek@selleo.com>
2022-04-06 15:03:02 +0200

    added redux to project

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import { store } from './store';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
